921045862853b46bb1c973601495ffa0237b9909
khalidelasrii <khalidelasri534@gmail.com>
2024-03-26 13:30:25 +0100

    to continue

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,5 @@
-function _auth(){
-    let _user=document.getElementById("email").value;
-    let _password = document.getElementById("pass").value;
+let boxLogin= document.getElementById("form-box-login");
+let boxRegistren = document.getElementById("form-box-register");
 
-    if(_user == "khalidelasri@gmail.com"&& _password=="123456"){
-        window.location.href ='../index.html';
-        alert("True");
-    }else{
-        alert("Email or Password Error");
-    }
 
-}
+
